7e92ad06068237fef821f13fc3e98ff3aa756582
pegasusCK <cky1026@gmail.com>
2017-05-17 14:35:17 -0700

    Lesson 82 IBOutlets and Custom Weather Cell

---
+++ RainyShinyCloudy/WeatherCell (2017-05-17T21_24_53.315).swift
@@ -0,0 +1,26 @@
+//
+//  WeatherCell.swift
+//  RainyShinyCloudy
+//
+//  Created by Chihkao Yu on 5/17/17.
+//  Copyright © 2017 Chihkao Yu. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherType: UILabel!
+    @IBOutlet weak var highTemp: UILabel!
+    @IBOutlet weak var lowTemp: UILabel!
+    
+    func configureCell(forecast: Forecast) {
+        lowTemp.text = "\(forecast.lowTemp)"
+        highTemp.text = forecast.highTemp
+        weatherType.text = forecast.weatherTpe
+        dayLabel.text = forecast.date
+        weatherIcon.image = UIImage(named: forecast._weatherType)
+    }
+}
